f2e987a8587ffd9050459fcb8ce35fb8b9e2039b
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-03 11:29:25 -0600

    display cards (temporary)

--- index.js
+++ index.js
@@ -1,66 +1,11 @@
-let dealerSum = 0;
-let playerSum = 0;
-
-function deckOfCards(){
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    let suits = ["S", "D", "C", "H"]
-}
-
-
-
-function newGame(){
-    
-}
-
-function hit(){}
-
-function stand(){}
-
-function deal(){}
-
-function dealerHand(){}
-
-function playerHand(){}
-
-function showCards(){}
-
-function showDealerHand(){}
-
-function showPlayerHand(){}
-
-function showDealerSum(){}
-
-function showPlayerSum(){}
-
-function showResult(){}
-
-function reset(){}
-
-function resetDealer(){}
-
-function resetPlayer(){}
-
-function resetDealerSum(){}
-
-function resetPlayerSum(){}
-
-function resetResult(){}
-
-
-
-
-
-
-
-
-
-fetch('https://deckofcardsapi.com/')
+fetch('http://localhost:3000/cards')
  .then(response => response.json())
- .then(data => console.log(data))
+ .then(data => {
+    data.forEach(displayCard)
+ })
 
-
- function shuffleCards() {
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then(response => response.json())
-    .then(cards => console.log(cards))
+ function displayCard(card){
+    const newCard = document.createElement("img")
+    newCard.src = card.image
+    document.querySelector("body").append(newCard)
  }
